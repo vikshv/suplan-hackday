50f73a685a3cc6c79c524d232968a48e677d5714
Viktor Shvetcov <vikshv@yandex.ru>
2017-10-29 15:52:40 +0300

    add chart

--- src/app/statistic-tab.component.ts
+++ src/app/statistic-tab.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
+import { PlanService } from './plan.service';
 
 @Component({
     selector: 'statistic-tab',
@@ -7,9 +7,90 @@ import { AuthService } from './auth.service';
     styleUrls: [ './statistic-tab.component.css' ]
 })
 export class StatisticTabComponent implements OnInit {
-    progress: boolean = false;
-    constructor(public auth: AuthService) {
+
+    constructor(public planService: PlanService) {
     }
+
     ngOnInit(): void {
+        const AmCharts = window['AmCharts'];
+        const { days } = this.planService.plan;
+
+        const dataProvider = days.map((day, index) => {
+            const calories = this.reduceProp(day, 'calories');
+            const weight = this.reduceProp(day, 'weight');
+            return {
+                "category": `category ${index}`,
+                "column-1": weight.toFixed(2),
+                "column-2": calories.toFixed(2)
+            };
+        });
+
+        AmCharts.makeChart("chartdiv",
+                {
+                    "type": "serial",
+                    "categoryField": "category",
+                    "startDuration": 1,
+                    "categoryAxis": {
+                        "gridPosition": "start"
+                    },
+                    "trendLines": [],
+                    "graphs": [
+                        {
+                            "balloonText": "[[title]] of [[category]]:[[value]]",
+                            "fillAlphas": 1,
+                            "id": "AmGraph-1",
+                            "title": "graph 1",
+                            "type": "column",
+                            "valueField": "column-1",
+                            "labelText": "[[value]]"
+                        },
+                        {
+                            "balloonText": "[[title]] of [[category]]:[[value]]",
+                            "fillAlphas": 1,
+                            "id": "AmGraph-2",
+                            "title": "graph 2",
+                            "type": "column",
+                            "valueField": "column-2",
+                            "labelText": "[[value]]"
+                        }
+                    ],
+                    "guides": [],
+                    "valueAxes": [
+                        {
+                            "id": "ValueAxis-1",
+                            "stackType": "regular",
+                            "title": "Axis title"
+                        }
+                    ],
+                    "allLabels": [],
+                    "balloon": {},
+                    "legend": {
+                        "enabled": true,
+                        "useGraphSettings": true
+                    },
+                    "titles": [
+                        {
+                            "id": "Title-1",
+                            "size": 15,
+                            "text": "Chart Title"
+                        }
+                    ],
+                    "dataProvider": dataProvider
+                }
+            );
+    }
+
+    reduceProp(day, name) {
+        return day.repasts ? day.repasts.reduce((result, repast) => {
+            return result + this._reduceRecipes(repast, name);
+        }, 0) : 0;
+    }
+
+    _reduceRecipes(repast, name) {
+        return repast.recipes ? repast.recipes.reduce((result, recipe) => {
+            return recipe.products ? recipe.products.reduce((result, product) => {
+                return result + parseFloat(product[name] || 0);
+            }, result) : 0;
+        }, 0) : 0;
     }
 }
