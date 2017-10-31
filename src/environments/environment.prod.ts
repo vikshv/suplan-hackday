7a8b16c0f777812f49c79bdd402eea4ffa4c57a0
Viktor Shvetcov <vikshv@yandex.ru>
2017-10-31 09:39:55 +0300

    setup prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyC-s4F1CgE3yBOVz-Tt-1OGLO-Gq9oQc6c',
+        authDomain: 'suplan-e2275.firebaseapp.com',
+        databaseURL: 'https://suplan-e2275.firebaseio.com',
+        projectId: 'suplan-e2275',
+        storageBucket: '',
+        messagingSenderId: '443317421416'
+    }
 };
